3557c4d720a89ce70bbf8eb5412578f572a13fa1
Baba Zaha <dsmwaura@gmail.com>
2022-04-06 09:52:00 +0300

    On Create of healthcare practitioner create company a, usernd lilink/restrict practitioner to company

--- practice_manager/hooks.py
+++ practice_manager/hooks.py
@@ -95,13 +95,11 @@ app_license = "MIT"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-#	}
-# }
+doc_events = {
+	"Healthcare Practitioner": {
+		"after_insert": "practice_manager.pm_hooks.healthcare_practitioner.link_user_and_company",
+	}
+}
 
 # Scheduled Tasks
 # ---------------
