9585d77dfeef90b43037fb3610afc27cd6196be9
Philsnr <31213518+BigPhilsnr@users.noreply.github.com>
2023-04-09 21:28:26 +0300

    Update dasbhoard.py

--- practice_manager/api/dasbhoard.py
+++ practice_manager/api/dasbhoard.py
@@ -2,9 +2,12 @@ import frappe
 @frappe.whitelist()
 def cash():
     user = frappe.session.user
-    invoices = 0
+ 
     result = frappe.db.sql(f"select SUM(amount) as amount from `tabConsult Ledger` where owner ='{user}' ", as_dict = 1)
-    invoices = result[0].get('amount')
+    invoices = result[0].get('amount') or 0
+    
+    result = frappe.db.sql(f"select COUNT( DISTINCT facility) as amount from `tabConsult Ledger` where owner ='{user}' ", as_dict = 1)
+    facilities = result[0].get('amount') or 0
 
     user = frappe.session.user
     result = frappe.db.sql(f"select SUM(paid_amount) as amount from `tabPayment Entry` where owner ='{user}' ", as_dict=1)
@@ -16,4 +19,4 @@ def cash():
     result = frappe.db.sql(f"select SUM(name) as amount from `tabPatient Encounter` where owner ='{user}' ", as_dict = 1)
     patient_encounters = result[0].get('amount') or 0
 
-    return dict(invoices=invoices, payments = payments, patients = patients, patient_encounters = patient_encounters)
+    return dict(facilities = facilities, invoices=invoices, payments = payments, patients = patients, patient_encounters = patient_encounters)
