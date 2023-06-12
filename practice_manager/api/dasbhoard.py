6a1a23b877b8106aea1378aee0fe3b47265833a1
Philsnr <31213518+BigPhilsnr@users.noreply.github.com>
2023-06-13 01:46:05 +0300

    Update payment total fetch from company not user

--- practice_manager/api/dasbhoard.py
+++ practice_manager/api/dasbhoard.py
@@ -4,15 +4,16 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import get_outstanding
 @frappe.whitelist()
 def cash():
     user = frappe.session.user
- 
-    result = frappe.db.sql(f"select SUM(amount) as amount from `tabConsult Ledger` where owner ='{user}' ", as_dict = 1)
+    company = get_company(user)
+    result = frappe.db.sql(f"select SUM(amount) as amount from `tabConsult Ledger` where company ='{company}' ", as_dict = 1)
     invoices = result[0].get('amount') or 0
     
     result = frappe.db.sql(f"select COUNT( DISTINCT facility) as amount from `tabConsult Ledger` where owner ='{user}' ", as_dict = 1)
     facilities = result[0].get('amount') or 0
 
     user = frappe.session.user
-    result = frappe.db.sql(f"select SUM(paid_amount) as amount from `tabPayment Entry` where owner ='{user}' ", as_dict=1)
+    
+    result = frappe.db.sql(f"select SUM(paid_amount) as amount from `tabPayment Entry` where company ='{company}' ", as_dict=1)
     payments = result[0].get('amount') or 0
 
     result = frappe.db.sql(f"select COUNT(name) as amount from `tabPatient` where owner ='{user}'", as_dict = 1)
